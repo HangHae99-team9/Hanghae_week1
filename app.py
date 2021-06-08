7d1c9e48db7bdfc606661d72ce35baf215f7c508
icwgi <dhkdrb897@naver.com>
2021-06-08 15:20:29 +0900

    Revert "update 0608"
    This reverts commit 3aeec1059e8aafc112d472f6fa1b52bb21e14e2b.

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ import datetime
 # 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
 import hashlib
 
-import requests
 
 #################################
 ##  HTML을 주는 부분             ##
@@ -30,11 +29,13 @@ import requests
 @app.route('/')
 def main():
     msg = request.args.get("msg")
-    r = requests.get('http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=f02f0047514de8f2bca5b5c23374ee21&targetDt=20210601')
-    response = r.json()
-    weeklys = response['boxOfficeResult']['weeklyBoxOfficeList']
-    return render_template("main.html" , weeklys=weeklys, msg=msg)
+    return render_template('main.html', msg=msg)
 
+
+@app.route('/index')
+def index():
+    msg = request.args.get("msg")
+    return render_template('index.html', msg=msg)
 
 # def home():
 #     token_receive = request.cookies.get('mytoken')
@@ -60,9 +61,9 @@ def join():
     return render_template('join.html')
 
 
-@app.route('/review')
-def review():
-    return render_template('review.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 #################################
