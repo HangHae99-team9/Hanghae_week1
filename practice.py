2b23f4b2e229a4614cbf3e0be3bc7ba3adfbcf47
icwgi <dhkdrb897@naver.com>
2021-06-08 20:12:48 +0900

    자바스크립트 로그인페이지 수정

--- practice.py
+++ practice.py
@@ -3,7 +3,11 @@ from pymongo import MongoClient
 
 # 시간가져오기 코드
 import datetime
+#
+# now_time = datetime.datetime.now()
+# now_time = str(now_time)
+# print(now_time[:16])
 
-now_time = datetime.datetime.now()
-now_time = str(now_time)
-print(now_time[:16])
+
+ut = datetime.utcnow()
+print(ut)
