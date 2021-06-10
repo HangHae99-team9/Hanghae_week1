427b5b2ba6212bc2986a0f6c44b3fe70b44c7336
endol007 <37258336+endol007@users.noreply.github.com>
2021-06-10 19:09:02 +0900

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -6,7 +6,28 @@ function toggle_sign_up() {
             $("#help-password").toggleClass("hidden")
             $("#help-password2").toggleClass("hidden")
 }
-
+function kakaoshare() {
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "Hanghae_Office",
+      description: "설명",
+      imageUrl:
+        "http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+      link: {
+        webUrl: "http://hanghae-office.shop",
+      },
+    },
+    buttons: [
+      {
+        title: "웹으로 이동",
+        link: {
+          webUrl: "http://hanghae-office.shop",
+        },
+      },
+    ],
+  });
+}
 
 function is_nickname(asValue) {
     var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
